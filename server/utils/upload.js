7a5fe202959564c8176667e06a2f838a0d688086
Abhishek Narula <abhisheknarula05@gmail.com>
2023-09-10 23:42:30 +0530

    Bharat intern

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 import { GridFsStorage } from 'multer-gridfs-storage';
 
 const storage = new GridFsStorage({
-    url: `mongodb://abhishek:abhishek@ac-8ppyy19-shard-00-00.vjk3nc9.mongodb.net:27017,ac-8ppyy19-shard-00-01.vjk3nc9.mongodb.net:27017,ac-8ppyy19-shard-00-02.vjk3nc9.mongodb.net:27017/?ssl=true&replicaSet=atlas-tb0uns-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    url: `mongodb://<user>:<password>@ac-8ppyy19-shard-00-00.vjk3nc9.mongodb.net:27017,ac-8ppyy19-shard-00-01.vjk3nc9.mongodb.net:27017,ac-8ppyy19-shard-00-02.vjk3nc9.mongodb.net:27017/?ssl=true&replicaSet=atlas-tb0uns-shard-0&authSource=admin&retryWrites=true&w=majority`,
     options: { useNewUrlParser: true },
     file: (request, file) => {
         const match = ["image/png", "image/jpg"];
